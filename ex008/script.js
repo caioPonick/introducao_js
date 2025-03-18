06f2dd41a613e1433230401f01d91d1415efcc8a
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-03-18 09:50:56 -0300

    ADD - ex008

---
+++ ex008/script.js
@@ -0,0 +1,6 @@
+function media(n1, n2, n3) {
+    let soma = Number(n1) + Number(n2)+ Number(n3);
+    let div = soma/3;
+    alert("A média é " + div);
+}
+media(prompt("Digite um número: "), prompt("Digite outro número: "), prompt("Digite um último número: "));
